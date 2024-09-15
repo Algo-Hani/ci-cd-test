2cf97e64f12edf2a8eb1d8ab667268d84c8565d8
Woong99 <dndry6852@gmail.com>
2024-09-15 15:49:01 +0900

    111
    111

--- module-common/src/main/java/algohani/common/component/HealthCheckComponent.java
+++ module-common/src/main/java/algohani/common/component/HealthCheckComponent.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 public class HealthCheckComponent {
 
     public String healthCheck() {
-        log.info("health check!!");
+        log.info("health check!!!");
         return "OK";
     }
 }
